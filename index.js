32e1b0e8ca4ef72d50f0b2f6c08a9f9bfab4315d
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-11-24 11:25:38 +0800

    First commit
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

---
+++ index.js
@@ -0,0 +1,75 @@
+var admin = require("firebase-admin");
+var Vision = require('@google-cloud/vision');
+var util = require('util');
+var fs = require('fs');
+
+// Instantiate a vision client
+var vision = Vision();
+
+admin.initializeApp({
+  credential: admin.credential.cert("config/firebase.json"),
+  databaseURL: "https://prototype-profiler.firebaseio.com",
+  storageBucket: "prototype-profiler.appspot.com",
+  databaseAuthVariableOverride: {
+    uid: "my-service-worker"
+  }
+});
+
+var db = admin.database();
+var ref = db.ref("/images");
+var processed = db.ref('/processed_images');
+ref.on("child_added", function(snapshot) {
+  var obj = snapshot.val();
+  var inputFile = obj.url;
+  var uid = obj.uid;
+  var email = obj.email;
+  var key = snapshot.key;
+
+  console.log(key)
+  
+
+  vision.detectFaces(inputFile, function (err, faces) {
+    if (err) {
+      console.log(err)
+    }
+    for (var i in faces) {
+      db.ref('/processed_images/' + key + '/faces/' + i).set(faces[i]);
+    }
+  });
+
+  vision.detectLabels(inputFile, { verbose: true }, function (err, labels) {
+    if (err) {
+      console.log(err)
+    }
+    for (var i in labels) {
+      db.ref('/processed_images/' + key + '/labels/' + i).set(labels[i]);
+    }
+  });
+
+  vision.detectText(inputFile, { verbose: true }, function(err, text) {
+    if (err) {
+      console.log(err)
+    }
+    for (var i in text) {
+      db.ref('/processed_images/' + key + '/text/' + i).set(text[i]);
+    }
+  })
+
+  vision.detectLandmarks(inputFile, { verbose: true }, function(err, landmarks) {
+    if (err) {
+        console.log(err)
+      }
+      for (var i in landmarks) {
+        db.ref('/processed_images/' + key + '/landmarks/' + i).set(landmarks[i]);
+      }
+  });
+
+  setTimeout(function() {
+    db.ref('/processed_images/' + key + '/data').set({
+      uid: uid,
+      email: email,
+      dateTaken: admin.database.ServerValue.TIMESTAMP
+    });
+    admin.database().ref("/images/"+key).remove();
+  }, 2000)
+});
